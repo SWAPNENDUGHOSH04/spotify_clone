2919b142c4abfe34863256a96ff8893019195302
SWAPNENDU GHOSH <134549608+SWAPNENDUGHOSH04@users.noreply.github.com>
2024-02-29 20:12:15 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains : ["aulkffykexfflvnxhqki.supabase.co"]
+    }
+}
+
+module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/(site)/page.tsx",
+  "./components/sidebar.tsx",
+  "./components/box.tsx",
+  "./components/sidebaritems.tsx",
+"./components/library.tsx",
+"./components/header.tsx",
+"./components/button.tsx",
+"./components/listitems.tsx",
+"./components/modal.tsx",
+"./components/uploadmodal.tsx",
+"./app/(site)/components/songitem.tsx",
+"./app/(site)/components/pagecontent.tsx",
+"./components/mediaitem.tsx",
+"./app/search/page.tsx",
+"./components/searchinput.tsx",
+"./app/search/components/searchcontent.tsx",
+"./components/likebutton.tsx",
+"./actions/getLikedSongs.ts",
+"./app/liked/page.tsx",
+"./components/player.tsx",
+"./components/playercontent.tsx",
+"./components/slider.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
